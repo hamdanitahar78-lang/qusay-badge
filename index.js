0c0acdfd6949b881d5f27004d9297c4cba0d925c
hamdanitahar78-lang <hamdanitahar78@gmail.com>
2026-08-25 01:35:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import { logger } from "@revenge-mod/plugin";
 
 export default {
   onLoad() {
-    logger.log("♡ QUSAYCORE badge loaded!");
+    logger.log("╭──────────────╮");
+    logger.log("♡ QUSAYCORE");
+    logger.log("♡ Qusay Badge is ON!");
+    logger.log("╰──────────────╯");
   },
 
   onUnload() {
-    logger.log("♡ QUSAYCORE badge unloaded!");
+    logger.log("♡ QUSAYCORE Badge OFF");
   }
 };
